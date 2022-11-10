c08940ecad822d14ddb0a7aaa83c8e3944ac938c
Arun-Jawlia <arunkumar08.mk@gmail.com>
2022-11-10 20:18:11 +0530

    checkout and payment page

--- wiggly-stick-3716/src/App.js
+++ wiggly-stick-3716/src/App.js
@@ -2,12 +2,14 @@
 import './App.css';
 
 import Checkout from './components/Checkout/Checkout';
+import Payment from './components/Payment/Payment';
 import MostorizerPage from './Pages/MostorizerPage';
 
 function App() {
   return (
     <div className="App">
         <Checkout/>
+        <Payment/>
         <MostorizerPage/>
     </div>
   );
