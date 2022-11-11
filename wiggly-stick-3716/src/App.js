dfc6a7dfbbebed138800990816d7132356166cbf
Arun-Jawlia <arunkumar08.mk@gmail.com>
2022-11-11 21:54:28 +0530

    new update

--- wiggly-stick-3716/src/App.js
+++ wiggly-stick-3716/src/App.js
@@ -1,51 +1,12 @@
 import "./App.css";
-// <<<<<<< HEAD
-// import Navbar from "../src/Components/Navbar/Navbar.jsx"
 
-
-import './App.css';
-import FoundationCard from './Components/Foundation/FoundationCard';
-import ProductShow from './Components/Foundation/ProductShow';
-import FoundationPage from './Pages/Foundation/FoundationPage';
-import MainRoute from './Pages/Foundation/MainRoutes';
-
-
-
-
-// import Home from "./Pages/Home"
-// import ScrollButton from "./Components/Scrollbutton";
-// import Checkout from "./Components/Checkout/Checkout";
-// import Payment from "./Components/Payment/Payment";
-// import MostorizerPage from "./Pages/MostorizerPage";
-// import Navbar from "./Components/Navbar/Navbar";
-// import Address from "./Components/AddressPage/Address";
 import AllRoutes from "./Routes/AllRoutes";
 function App() {
   return (
-    <div className="App">
-      <AllRoutes/>
-      </div>
-  ) 
-} 
-// =======
-// import Checkout from "./Components/Checkout/Checkout";
-// import Payment from "./Components/Payment/Payment";
-// import Navbar from "./Components/Navbar/Navbar";
-
-// import Allroutes from "./Pages/Allroutes";
-// function App() {
-//   return (
-//     <div className="App">
-//       <Navbar />
-//       <Home/>
-//       <Address/>
-//       <Checkout />
-//       <Payment />
-     
-//       <Allroutes/>
-//     </div>
-//   )
-// }
-
+    <div >
+      <AllRoutes />
+    </div>
+  );
+}
 
 export default App;
--- wiggly-stick-3716/src/Routes/AllRoutes.jsx
+++ wiggly-stick-3716/src/Routes/AllRoutes.jsx
@@ -7,6 +7,8 @@ import Checkout from '../Components/Checkout/Checkout'
 import Address from '../Components/AddressPage/Address'
 import Payment from '../Components/Payment/Payment'
 import MostorizerPage from '../Pages/MostorizerPage'
+import Singleitemshow from '../Pages/Singleitemshow'
+import FoundationPage from '../Pages/Foundation/FoundationPage'
 
 const AllRoutes = () => {
   return (
@@ -15,12 +17,14 @@ const AllRoutes = () => {
         <Route path='/login' element={<Login/>}/>
         <Route path='/signup' element={<Signup/>}/>
         <Route path='/moisturizer' element={<MostorizerPage/>}/>
-        <Route path='/foundation' element={<h1>Foundation</h1>} />
+        <Route path='/foundation' element={<FoundationPage/>} />
         <Route path='/checkout' element={<Checkout/>}/>
         <Route path='/address' element={<Address/>}/>
         <Route path='/payment' element={<Payment/>}/>
         <Route path='/cart' element={<h1>Cart</h1>}/>
         <Route path='/favourite' element={<h1>Favourite</h1>}/>
+        {/* <Route path = "/products/:productId" element = {<Singleitemshow/>}/> */}
+        <Route path = "/moisturizer/:productId" element = {<Singleitemshow/>}/>
 
     </Routes>
   )
